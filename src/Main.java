f158693e7000ad371872afbdc05d45ce7f139573
Ke1en <Andpey-dok@mail.ru>
2024-09-23 22:25:34 +0600

    Project created

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        byte byteNum = 2; // 1 байт
+        short shortNum = 34; // 2 байта
+        int number = 1; // 4 байта
+        long longNum = 102354; // 8 байт
+
+        float floatNum = 20f;
+        char symbol = 'f';
+        boolean isNumberInt = false;
+        String stroke = "This is string";
+    }
+}
